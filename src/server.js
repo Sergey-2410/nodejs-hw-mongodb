509dd62a5135131e93eb2156f17e636b7cc74a11
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2025-01-18 00:34:12 +0200

    fix server.js

--- src/server.js
+++ src/server.js
@@ -16,9 +16,6 @@ export const setupServer = () => {
     }),
   );
   app.use(cors());
-  app.use('*', (req, res, next) => {
-    res.status(404).json({ message: 'Not found' });
-  });
 
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
@@ -46,6 +43,9 @@ export const setupServer = () => {
     });
   });
 
+  app.use('*', (req, res, next) => {
+    res.status(404).json({ message: 'Not found' });
+  });
   app.use((err, req, res, next) => {
     res.status(500).json({
       message: 'Something went wrong',
