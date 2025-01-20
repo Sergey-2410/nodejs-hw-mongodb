d1fd17bdf1962e61c469a00570fc527b6440163e
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2025-01-20 21:47:13 +0200

    update hw3

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,3 +7,22 @@ export const getContactById = async (contactId) => {
   const contact = await contactsCollection.findById(contactId);
   return contact;
 };
+export const createContact = async (payload) => {
+  const contact = await contactsCollection.create(payload);
+  return contact;
+};
+export const updateContact = async (contactId, payload, options = {}) => {
+  const result = await contactsCollection.findOneAndUpdate(
+    { id: contactId },
+    payload,
+    {
+      new: true,
+      ...options,
+    },
+  );
+  return result;
+};
+export const deleteContact = async (contactId) => {
+  const result = await contactsCollection.findOneAndDelete({ id: contactId });
+  return result;
+};
