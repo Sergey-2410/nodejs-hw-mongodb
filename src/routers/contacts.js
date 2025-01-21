c9f569552a804a1940495f3e26ef926a0a85c4b7
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2025-01-21 11:33:08 +0200

    fix router delete

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -14,5 +14,5 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 router.post('/contacts', ctrlWrapper(createContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
-router.delete('/contacts/:contact', ctrlWrapper(deleteContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 export default router;
