afcb225922ac1b22f4c9b5fd1d5f378cfe2e0b22
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2025-01-28 22:23:35 +0200

    fix createContactSchema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -5,10 +5,7 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string(),
   isFavourite: Joi.boolean().default(false),
-  contactType: Joi.string()
-    .valid('work', 'home', 'personal')
-    .required()
-    .default('personal'),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
 export const updateContactSchema = Joi.object({
