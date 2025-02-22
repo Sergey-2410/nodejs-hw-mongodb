26e12dc6deffb86fafb36c6e142a3522b42c831e
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2025-02-22 22:25:41 +0200

    add swagger

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 export const setupServer = () => {
@@ -22,10 +23,11 @@ export const setupServer = () => {
   app.use(cors());
   app.use(cookieParser());
   app.use(router);
+  app.use('/upload', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
   app.use(errorHandler);
-  app.use('/upload', express.static(UPLOAD_DIR));
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
